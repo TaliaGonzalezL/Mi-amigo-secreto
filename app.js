de2317528e4c9f9cbcb7f56d51ab6439f1722d28
TaliaGonzalezL <talia.golo2@gmail.com>
2025-03-09 17:47:54 -0600

    Add files via upload
    Proyecto final. 
    En esta versión, primero nos aseguramos que el usuario ingresa un nombre, añadimos ese nombre en el array, sorteamos un nombre de esa lista de array, y se imprime en el sitio web el nombre del amigo sorteado

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-// Autor: Talia González López
-
-
-let numeroMaximoNombresRecibidos = 0;
-let listaAmigosAgregados = [];
-let amigoSorteado = 0
-
-
-function agregarNombre () {
-    let nombreAgregado = document.getElementById('amigoRecibido').value;
-    listaAmigosAgregados.push(nombreAgregado);
-    console.log(nombreAgregado);
-    console.log(listaAmigosAgregados.length);
-    limpiarCaja(); 
+let amigo = [] ;
+function agregarAmigo () {
+    let inputAmigo = document.getElementById("amigo");
+    let nombreAmigo = inputAmigo.value;
+    
+    if (!nombreAmigo) {
+        alert("Debes ingresar un nombre");
+        return;
+    }
+    amigo.push(nombreAmigo);
+    inputAmigo.value = "";
+    inputAmigo.focus ();
+    listaAmigos();
 }
+//hacer que el nombre aparezca en la pantalla
+function listaAmigos () {
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
 
-function limpiarCaja () {
-    document.getElementById('amigoRecibido').value=' ';
+    for(let i = 0; i < amigo.length; i++) {
+        let item = document.createElement ("li");
+        item.textContent = amigo[i];
+        listaAmigos.appendChild(item);
+    }
+//sortear amigos, sacar un amigo al azar
+} function sortearAmigo () {
+    if (amigo.length === 0) {
+        alert("No hay amigos para sortear");
+        return;
+    }
+    let amigoSorteado = amigo[Math.floor(Math.random()* amigo.length)];
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `El amigo sorteado es: ${amigoSorteado}`;
+
 }
-
-function sortearAmigo() {
-    let amigoSorteado = Math.floor(Math.random()*(listaAmigosAgregados.length+1));
-    console.log(listaAmigosAgregados[amigoSorteado]);
-}
-
